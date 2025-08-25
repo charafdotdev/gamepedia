467b8ad87b18d8806ea5ccb4c21c805e53293774
Charaf Dev <charafeddine.dev@gmail.com>
2025-08-25 17:09:52 +0100

    Building game cards.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
 import { CanceledError } from 'axios';
 
-interface Game {
+export interface Game {
   id: number;
   title: string;
+  thumbnail: string;
 }
 
 type FetchGamesResponse = Game[];
